7f156eb5013467dd3f6ab29b4d7047661fa67171
Philipp W <phil.w07@gmail.com>
2022-12-25 04:11:21 +0100

    Implement messages

---
+++ aiokonstsmide/message.py
@@ -0,0 +1,203 @@
+"""Defines messages to be sent to a device."""
+
+from datetime import datetime
+from enum import Enum
+from functools import reduce
+
+
+MAGIC_BYTE = 0xBC
+
+
+class Command(Enum):
+    OnOff = 1
+    Control = 2
+    SetPassword = 3
+    PasswordInput = 4
+    Timer = 5
+    Rtc = 6
+
+
+class Function(Enum):
+    Keep = 0
+    Combination = 1
+    InWaves = 2
+    Sequential = 3
+    SloGlo = 4
+    Chasing = 5
+    SlowFade = 6
+    Twinkle = 7
+    Steady = 8
+    FlashAlternating = 9
+    FlashSynchronous = 10
+
+
+class Repeat(Enum):
+    Sunday = 1
+    Monday = 2
+    Tuesday = 4
+    Wednesday = 8
+    Thursday = 16
+    Friday = 32
+    Saturday = 64
+
+
+def on_off(on: bool) -> list[int]:
+    """Constructs a message to turn the device on or off."""
+    return bytes(
+        [
+            MAGIC_BYTE,
+            Command.OnOff.value,
+            int(on),
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ]
+    )
+
+
+def control(function: Function, brightness: int, flash_speed: int) -> list[int]:
+    """
+    Constructs a message to control the function, brightness and flash speed of the device.
+
+    The flash speed only affects the alternating and synchronous flash functions.
+    """
+    if not function:
+        raise ValueError("A valid function must be given")
+    if function == Function.Keep:
+        raise ValueError("Keep function can only be used with timers")
+    if brightness is None or not (0 <= brightness <= 100):
+        raise ValueError(f"Brightness must be between 0 and 100, got {brightness}")
+    if flash_speed is None or not (0 <= flash_speed <= 100):
+        raise ValueError(f"Flash speed must be between 0 and 100, got {flash_speed}")
+
+    return bytes(
+        [
+            MAGIC_BYTE,
+            Command.Control.value,
+            function.value,
+            brightness,
+            flash_speed,
+            0,
+            0,
+            0,
+            0,
+        ]
+    )
+
+
+def password_input(password: str) -> list[int]:
+    """
+    Constructs a password input message.
+    The password must consist of exactly six digits.
+
+    This message has to be sent to the device first with the correct password to be able to control it.
+    """
+    if not password or len(password) != 6 or not password.isdigit():
+        raise ValueError("The password must consist of exactly six digits")
+
+    num = int(password)
+    return bytes(
+        [
+            MAGIC_BYTE,
+            Command.PasswordInput.value,
+            (num >> 24) & 0xFF,
+            (num >> 16) & 0xFF,
+            (num >> 8) & 0xFF,
+            num & 0xFF,
+            0,
+            0,
+            0,
+        ]
+    )
+
+
+def set_password(password: str) -> list[int]:
+    """
+    Constructs a set password message.
+    The password must consist of exactly six digits.
+    """
+    if not password or len(password) != 6 or not password.isdigit():
+        raise ValueError("The password must consist of exactly six digits")
+
+    num = int(password)
+    return bytes(
+        [
+            MAGIC_BYTE,
+            Command.SetPassword.value,
+            (num >> 24) & 0xFF,
+            (num >> 16) & 0xFF,
+            (num >> 8) & 0xFF,
+            num & 0xFF,
+            0,
+            0,
+            0,
+        ]
+    )
+
+
+def timer(
+    num: int,
+    active: bool,
+    turn_on: bool,
+    hour: int,
+    minute: int,
+    function: Function,
+    repeat: list[Repeat],
+    brightness: int,
+):
+    """Constructs a timer message."""
+    if not (0 <= num <= 7):
+        raise ValueError(f"Timer number must be between 0 and 7, got {num}")
+    if not (0 <= hour <= 23):
+        raise ValueError(f"Hour must be between 0 and 23, got {hour}")
+    if not (0 <= minute <= 59):
+        raise ValueError(f"Minute must be between 0 and 59, got {minute}")
+    if not function:
+        raise ValueError("A valid function must be given")
+    if function in [Function.FlashAlternating, Function.FlashSynchronous]:
+        raise ValueError("Flash functions can't be used with timers")
+    if brightness is None or not (0 <= brightness <= 100):
+        raise ValueError(f"Brightness must be between 0 and 100, got {brightness}")
+
+    if repeat is None or len(repeat) == 0:
+        repeat = [0]
+    else:
+        repeat = [rep.value for rep in repeat]
+
+    return bytes(
+        [
+            MAGIC_BYTE,
+            Command.Timer.value,
+            num,
+            int(turn_on),
+            int(active),
+            hour,
+            minute,
+            reduce(lambda a, b: a ^ b, repeat),
+            function.value,
+            brightness,
+        ]
+    )
+
+
+def rtc(date: datetime):
+    """
+    Constructs an RTC message to synchronize the date and time of the device.
+    This is necessary for the timers to work correctly.
+    """
+    return bytes(
+        [
+            MAGIC_BYTE,
+            Command.Rtc.value,
+            date.second,
+            date.minute,
+            date.hour,
+            date.day,
+            date.month,
+            date.year & 0xFF,
+            (date.year >> 8) & 0xFF,
+        ]
+    )
---
+++ tests/test_message.py
@@ -0,0 +1,227 @@
+from datetime import datetime
+
+import pytest
+
+from aiokonstsmide import message
+
+
+def test_on_off():
+    assert message.on_off(True) == b"\xBC\x01\x01\x00\x00\x00\x00\x00\x00"
+    assert message.on_off(False) == b"\xBC\x01\x00\x00\x00\x00\x00\x00\x00"
+
+
+def test_control():
+    # Valid values
+    tests = [
+        (
+            message.Function.Combination,
+            0x23,
+            0x44,
+            b"\xBC\x02\x01\x23\x44\x00\x00\x00\x00",
+        ),
+        (message.Function.InWaves, 0x01, 0x00, b"\xBC\x02\x02\x01\x00\x00\x00\x00\x00"),
+        (
+            message.Function.Sequential,
+            0x00,
+            0x08,
+            b"\xBC\x02\x03\x00\x08\x00\x00\x00\x00",
+        ),
+        (message.Function.SloGlo, 0x64, 0x12, b"\xBC\x02\x04\x64\x12\x00\x00\x00\x00"),
+        (message.Function.Chasing, 0x63, 0x64, b"\xBC\x02\x05\x63\x64\x00\x00\x00\x00"),
+        (
+            message.Function.SlowFade,
+            0x19,
+            0x55,
+            b"\xBC\x02\x06\x19\x55\x00\x00\x00\x00",
+        ),
+        (message.Function.Twinkle, 0x37, 0x01, b"\xBC\x02\x07\x37\x01\x00\x00\x00\x00"),
+        (message.Function.Steady, 0x45, 0x63, b"\xBC\x02\x08\x45\x63\x00\x00\x00\x00"),
+        (
+            message.Function.FlashAlternating,
+            0x57,
+            0x50,
+            b"\xBC\x02\x09\x57\x50\x00\x00\x00\x00",
+        ),
+        (
+            message.Function.FlashSynchronous,
+            0x0F,
+            0x28,
+            b"\xBC\x02\x0A\x0F\x28\x00\x00\x00\x00",
+        ),
+    ]
+    for fun, bri, spd, res in tests:
+        assert message.control(fun, bri, spd) == res
+
+    # Invalid values
+    with pytest.raises(ValueError):
+        message.control(message.Function.Combination, -1, 20)
+    with pytest.raises(ValueError):
+        message.control(message.Function.InWaves, 101, 33)
+
+    with pytest.raises(ValueError):
+        message.control(message.Function.FlashSynchronous, 79, -1)
+    with pytest.raises(ValueError):
+        message.control(message.Function.Sequential, 56, 101)
+
+    with pytest.raises(ValueError):
+        message.control(None, 79, 12)
+    with pytest.raises(ValueError):
+        message.control(message.Function.Keep, 79, 12)
+    with pytest.raises(ValueError):
+        message.control(message.Function.Chasing, None, 12)
+    with pytest.raises(ValueError):
+        message.control(message.Function.SloGlo, 79, None)
+
+
+def test_password_input():
+    # Valid values
+    assert message.password_input("123456") == b"\xBC\x04\x00\x01\xE2\x40\x00\x00\x00"
+    assert message.password_input("000000") == b"\xBC\x04\x00\x00\x00\x00\x00\x00\x00"
+    assert message.password_input("999999") == b"\xBC\x04\x00\x0F\x42\x3F\x00\x00\x00"
+    assert message.password_input("650238") == b"\xBC\x04\x00\x09\xEB\xFE\x00\x00\x00"
+
+    # Invalid values
+    with pytest.raises(ValueError):
+        message.password_input("1234567")
+    with pytest.raises(ValueError):
+        message.password_input(None)
+    with pytest.raises(ValueError):
+        message.password_input("")
+    with pytest.raises(ValueError):
+        message.password_input("-123")
+
+
+def test_set_password():
+    assert message.set_password("123456") == b"\xBC\x03\x00\x01\xE2\x40\x00\x00\x00"
+    assert message.set_password("000000") == b"\xBC\x03\x00\x00\x00\x00\x00\x00\x00"
+    assert message.set_password("999999") == b"\xBC\x03\x00\x0F\x42\x3F\x00\x00\x00"
+    assert message.set_password("650238") == b"\xBC\x03\x00\x09\xEB\xFE\x00\x00\x00"
+
+    # Invalid password
+    with pytest.raises(ValueError):
+        message.set_password("1234567")
+    with pytest.raises(ValueError):
+        message.set_password(None)
+    with pytest.raises(ValueError):
+        message.set_password("")
+    with pytest.raises(ValueError):
+        message.set_password("-123")
+
+
+def test_timer():
+    # Valid values
+    assert (
+        message.timer(
+            0,
+            True,
+            False,
+            12,
+            12,
+            message.Function.InWaves,
+            [message.Repeat.Saturday, message.Repeat.Sunday],
+            100,
+        )
+        == b"\xBC\x05\x00\x00\x01\x0C\x0C\x41\x02\x64"
+    )
+    assert (
+        message.timer(
+            1,
+            False,
+            True,
+            16,
+            57,
+            message.Function.Sequential,
+            [
+                message.Repeat.Monday,
+                message.Repeat.Tuesday,
+                message.Repeat.Wednesday,
+                message.Repeat.Thursday,
+                message.Repeat.Friday,
+            ],
+            53,
+        )
+        == b"\xBC\x05\x01\x01\x00\x10\x39\x3E\x03\x35"
+    )
+    assert (
+        message.timer(
+            6,
+            True,
+            True,
+            14,
+            3,
+            message.Function.Keep,
+            [message.Repeat.Monday, message.Repeat.Tuesday, message.Repeat.Sunday],
+            10,
+        )
+        == b"\xBC\x05\x06\x01\x01\x0E\x03\x07\x00\x0A"
+    )
+    assert (
+        message.timer(
+            6,
+            True,
+            True,
+            14,
+            3,
+            message.Function.Keep,
+            [],
+            10,
+        )
+        == b"\xBC\x05\x06\x01\x01\x0E\x03\x00\x00\x0A"
+    )
+    assert (
+        message.timer(
+            6,
+            True,
+            True,
+            14,
+            3,
+            message.Function.Keep,
+            None,
+            10,
+        )
+        == b"\xBC\x05\x06\x01\x01\x0E\x03\x00\x00\x0A"
+    )
+
+    # Invalid values
+    with pytest.raises(ValueError):
+        message.timer(-1, True, True, 1, 1, message.Function.Chasing, [], 100)
+    with pytest.raises(ValueError):
+        message.timer(9, True, True, 1, 1, message.Function.Chasing, [], 100)
+
+    with pytest.raises(ValueError):
+        message.timer(0, True, True, -1, 1, message.Function.Chasing, [], 100)
+    with pytest.raises(ValueError):
+        message.timer(0, True, True, 24, 1, message.Function.Chasing, [], 100)
+    with pytest.raises(ValueError):
+        message.timer(0, True, True, 1, -1, message.Function.Chasing, [], 100)
+    with pytest.raises(ValueError):
+        message.timer(0, True, True, 1, 60, message.Function.Chasing, [], 100)
+
+    with pytest.raises(ValueError):
+        message.timer(0, True, True, 1, 1, message.Function.FlashAlternating, [], 100)
+    with pytest.raises(ValueError):
+        message.timer(0, True, True, 1, 1, message.Function.FlashSynchronous, [], 100)
+
+    with pytest.raises(ValueError):
+        message.timer(0, True, True, 1, 1, message.Function.Chasing, [], -1)
+    with pytest.raises(ValueError):
+        message.timer(0, True, True, 1, 1, message.Function.Chasing, [], 101)
+
+
+def test_rtc():
+    assert (
+        message.rtc(datetime(2022, 11, 4, 9, 19, 27))
+        == b"\xBC\x06\x1B\x13\x09\x04\x0B\xE6\x07"
+    )
+    assert (
+        message.rtc(datetime(2013, 1, 1, 1, 7, 6))
+        == b"\xBC\x06\x06\x07\x01\x01\x01\xDD\x07"
+    )
+    assert (
+        message.rtc(datetime(2023, 12, 31, 23, 59, 58))
+        == b"\xBC\x06\x3A\x3B\x17\x1F\x0C\xE7\x07"
+    )
+    assert (
+        message.rtc(datetime(2048, 5, 24, 16, 43, 0))
+        == b"\xBC\x06\x00\x2B\x10\x18\x05\x00\x08"
+    )
