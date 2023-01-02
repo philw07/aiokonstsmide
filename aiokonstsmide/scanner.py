67b49363843421319efa35ea5d68eecb338f8bec
Philipp W <phil.w07@gmail.com>
2023-01-03 00:28:59 +0100

    Fix scanner module

--- aiokonstsmide/scanner.py
+++ aiokonstsmide/scanner.py
@@ -13,8 +13,8 @@ async def find_devices(timeout: float = 5.0) -> List[str]:
     """
     Scans for available Konstsmide Bluetooth devices.
     """
-    async for device in BleakScanner.discover(timeout=timeout, return_adv=False):
-        if device.name.strip().lower() == DEVICE_NAME:
+    for device in await BleakScanner.discover(timeout=timeout, return_adv=False):
+        if device.name and device.name.strip().lower() == DEVICE_NAME:
             yield device.address
 
 
@@ -23,6 +23,6 @@ async def check_address(address: str, timeout: float = 5.0) -> bool:
     Checks if the given address is a valid reachable Konstsmide device.
     """
     device = await BleakScanner.find_device_by_address(address, timeout=timeout)
-    if device and device.name.strip().lower() == DEVICE_NAME:
+    if device and device.name and device.name.strip().lower() == DEVICE_NAME:
         return True
     return False
